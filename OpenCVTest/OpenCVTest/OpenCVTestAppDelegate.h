299458a08e365b239a090669f4cd2f9430f38215
sonson <yoshida.yuichi@gmail.com>
2011-05-06 03:22:18 +0900

    Added the test code for OpenCV for iOS.

---
+++ OpenCVTest/OpenCVTest/OpenCVTestAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  OpenCVTestAppDelegate.h
+//  OpenCVTest
+//
+//  Created by sonson on 11/05/06.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OpenCVTestAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@property (nonatomic, retain) IBOutlet UINavigationController *navigationController;
+
+@end
---
+++ OpenCVTest/OpenCVTest/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  RootViewController.h
+//  OpenCVTest
+//
+//  Created by sonson on 11/05/06.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RootViewController : UITableViewController {
+
+}
+
+
+@end
