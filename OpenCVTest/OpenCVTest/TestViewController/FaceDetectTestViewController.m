5e7b3ebc1f87e7da1859a4b2be255d3b78d2acd2
sonson <yoshida.yuichi@gmail.com>
2011-05-29 09:43:09 +0900

    Arranged project file of OpenCVTest.

--- OpenCVTest/OpenCVTest/TestViewController/FaceDetectTestViewController.m
+++ OpenCVTest/OpenCVTest/TestViewController/FaceDetectTestViewController.m
@@ -34,14 +34,11 @@
 
 @implementation FaceDetectTestViewController
 
-- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
-{
-    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
-    if (self) {
-        // Custom initialization
-    }
-    return self;
++ (NSString*)testDescription {
+	return NSLocalizedString(@"Face detection", nil);
 }
+
+/*
 
 - (IplImage *)CreateIplImageFromUIImage:(UIImage *)image {
 	CGImageRef imageRef = image.CGImage;
@@ -118,44 +115,15 @@
 	cvReleaseHaarClassifierCascade(&cascade);
 }
 
+*/
+
 - (void)viewWillAppear:(BOOL)animated {
-	[super viewWillAppear:animated];
-	
-	[self test];
+	[super viewWillAppear:animated];	
+//	[self test];
 }
 
-- (void)dealloc
-{
+- (void)dealloc {
     [super dealloc];
-}
-
-- (void)didReceiveMemoryWarning
-{
-    // Releases the view if it doesn't have a superview.
-    [super didReceiveMemoryWarning];
-    
-    // Release any cached data, images, etc that aren't in use.
-}
-
-#pragma mark - View lifecycle
-
-- (void)viewDidLoad
-{
-    [super viewDidLoad];
-    // Do any additional setup after loading the view from its nib.
-}
-
-- (void)viewDidUnload
-{
-    [super viewDidUnload];
-    // Release any retained subviews of the main view.
-    // e.g. self.myOutlet = nil;
-}
-
-- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
-{
-    // Return YES for supported orientations
-    return (interfaceOrientation == UIInterfaceOrientationPortrait);
 }
 
 @end
